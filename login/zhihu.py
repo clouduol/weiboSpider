4c5c8e8b9b4a7a9a158ab37f715ca3ae23058e44
learnMachining <guoyunlong16@mails.ucas.ac.cn>
2017-03-05 19:42:59 +0800

    add cookie login function

--- login/zhihu.py
+++ login/zhihu.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # login zhihu
-# https email+password
+# https email+password ajax
 import requests
 import re
 import time
+import json
+import os
+import sys
 
 class Zhihu(object):
     # object data, public
@@ -27,34 +30,34 @@ class Zhihu(object):
     name = ""           # user nick name
     # user name regular expression
     reg_name = r"<span\s+?class=\"name\"[^>]*?>(.*?)</span>"
+    cookie_file = os.path.join(sys.path[0],"zhihu_cookie")    # file to store cookie
+    cookies = ""        # cookies
 
-    def __init__(self,email,password):
+    def __init__(self):
+        # change script directory as working directory
+        os.chdir(sys.path[0])
+        self.session = requests.session()
+        self.session.headers = self.headers
+
+    def login(self,email,password):
         self.email=email
         self.password=password
         self.data={"email":self.email,"password":self.password}
-        self.session = requests.session()
-
-    def login(self):
         # find _xsrf value
-        r = self.session.get(self.index_url,headers=self.headers)
-        print(r.url)
-        print(r.request.headers)
+        r = self.session.get(self.index_url)
         self.xsrf = re.findall(self.reg_xsrf,r.text)
         if len(self.xsrf)==0:
             print("Error! Can't find _xsrf value")
             return -1
         self.xsrf=self.xsrf[0]
         self.data['_xsrf']=self.xsrf
-        print(self.data)
         # time.sleep(5)
-        r = self.session.post(self.login_url,headers=self.headers,data=self.data)
-        print(r.url)
-        print(r.request.headers)
-        print(r.text.__class__)
-        print(r.text)
+        r = self.session.post(self.login_url,data=self.data)
         ret_dict = eval(r.text)
         if ret_dict['r'] == 0:
             print("Congratulations, login successfully!")
+            # store cookie, update in time
+            self.__store_cookie()
             return 0
         elif ret_dict['errcode'] == 100005:
             print("Email or password error, try again.")
@@ -63,25 +66,59 @@ class Zhihu(object):
             print("Someting wrong happened, login failed.")
             return -1
 
+    def login_by_cookie(self):
+        self.cookies = self.__load_cookie()
+        if self.cookies is None:
+            print("Load cookies error")
+            return -1
+        # set session cookie
+        self.session.cookies.update(self.cookies)
+        print("Load cookies from %s successfully!" % self.cookie_file)
+        return 0
+
     def get_username(self):
-        r = self.session.get(self.index_url,headers=self.headers)
+        r = self.session.get(self.index_url)
         self.name = re.findall(self.reg_name,r.text)
-        print(r.url)
-        print(r.request.headers)
-        print(self.name)
         if len(self.name) == 0:
             print("Someting wrong happened, get user name failed.")
             return None
         else:
             self.name = self.name[0]
-            print("User nick name: %s" % self.name)
+            print("Your nick name is: %s" % self.name)
             return self.name
 
+    # Serialize cookie to file
+    def __store_cookie(self):
+        with open(self.cookie_file,'w') as f:
+            cookies = self.session.cookies.get_dict()
+            json.dump(cookies,f)
+            print("Cookies stored into %s successfully!" % self.cookie_file)
+
+    # Deserialize cookie from file
+    def __load_cookie(self):
+        if os.path.exists(self.cookie_file):
+            with open(self.cookie_file,'r') as f:
+                cookies = json.load(f)
+                return cookies
+        else:
+            return None
+
+    # if exists cookie file
+    def find_cookie_file(self):
+        if os.path.exists(self.cookie_file):
+            return True
+        else:
+            return False
+
 if __name__ == "__main__":
-    print("Input your email:")
-    email = input()
-    print("Input your password:")
-    password = input()
-    zhihu = Zhihu(email,password)
-    zhihu.login()
+    zhihu = Zhihu()
+    if zhihu.find_cookie_file():
+        print("Login by cookie")
+        zhihu.login_by_cookie()
+    else:
+        print("Input your email:")
+        email = input()
+        print("Input your password:")
+        password = input()
+        zhihu.login(email,password)
     zhihu.get_username()
